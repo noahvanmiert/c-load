cb1c3d44c6b3a321e40d0cdefc40bf328bd33b7f
noahvanmiert <noahvanmiert1@gmail.com>
2023-06-15 22:16:48 +0200

    .gitignore and bin/

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,23 @@ use std::process::exit;
 
 fn init() {
     fs::create_dir("src-test").expect("Failed to create src/");
-    let mut file: File = File::create("src-test/main.c").expect("Failed to create src/main.c");
+    fs::create_dir("bin").expect("Failed to create bin/");
+
+    let mut src_file: File = File::create("src-test/main.c").expect("Failed to create src/main.c");
 
     let entry_point_template: &str = "#include <stdio.h>\n\nint main()\n{\n\tprintf(\"Hello, World!\");\n}\n";
 
     /* Write the main function to src/main.c */
-    file.write_all(entry_point_template.as_bytes()).expect("Faild to write to src/main.c");
+    src_file.write_all(entry_point_template.as_bytes()).expect("Faild to write to src/main.c");
+
+    let mut gitignore_file: File = File::create(".gitignore-test").expect("Failed to create .gitignore");
+    let gitignore_file_template: &str = "bin/";
+    gitignore_file.write_all(gitignore_file_template.as_bytes()).expect("Failed to write to .gitignore");
 }
 
 
 fn help() {
-    println!("Simple package manager for c\n");
+    println!("Simple package manager for C\n");
     println!("Usage: cload [OPTIONS] [COMMAND]\n");
     println!("Options:");
     println!("  -h, --help  Prints this help message\n");
